9913f7c4ea2726887a4736569ee91230b7bb5bad
Lucas Cavalcante <87142172+cavalcantedl@users.noreply.github.com>
2023-01-16 14:24:39 -0300

    Adiciona Components, Services e modules

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './services/calculadora.service';
+
+
+
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  exports: [
+    CalculadoraComponent
+
+  ],
+
+  imports: [
+    CommonModule
+  ], 
+  providers: [
+    CalculadoraService
+  ]
+  
+})
+export class CalculadoraModule { }
